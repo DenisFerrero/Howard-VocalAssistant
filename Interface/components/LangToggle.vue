d6f5a0abd20d1f35c19e5cd530c406c8c77313c0
Denis Ferrero <ferrero.denis@protonmail.com>
2021-04-03 15:47:26 +0200

    Added Nuxt + i18n

---
+++ Interface/components/LangToggle.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-dropdown variant="light">
+      <template #button-content>
+        {{ currentLocale }}
+        <country-flag :country="getRegionByCode(currentLocale)"></country-flag>
+      </template>
+      <b-dropdown-item
+        v-for="locale in availableLocales"
+        :key="locale.code"
+        :to="switchLocalePath(locale.code)"
+        @click.prevent.stop="$i18n.setLocale(locale.code)"
+      >
+        {{ locale.iso }}
+        <!-- Show the flag -->
+        <country-flag :country="getRegionByCode(locale.code)"></country-flag>
+      </b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+import CountryFlag from 'vue-country-flag'
+
+export default {
+  name: 'LangToggle',
+  components: {
+    CountryFlag,
+  },
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    },
+    currentLocale() {
+      return this.$i18n.locale
+    },
+  },
+  mounted() {
+    // eslint-disable-next-line no-console
+    console.log(this.$i18n.locales)
+  },
+  methods: {
+    getRegionByCode(code) {
+      return code.split('-')[1]
+    },
+  },
+}
+</script>
---
+++ Interface/nuxt.config.js
@@ -0,0 +1,61 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Interface',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: ['~/plugins/fontawesome.js'],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    'nuxt-i18n',
+    'nuxt-socket-io',
+  ],
+
+  // i18n configuration
+  i18n: {
+    langDir: '~/locales/',
+    // Default language based on user browser and saved in i18n_language cookie
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_language',
+      // Trigger the language detection only on homepage
+      onlyOnRoot: true,
+    },
+    defaultLocale: 'en-US',
+    locales: [
+      { code: 'en-US', iso: 'en-US', file: 'en-US.js', dir: 'ltr' },
+      { code: 'it-IT', iso: 'it-IT', file: 'it-IT.js', dir: 'ltr' },
+    ],
+    lazy: true,
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
