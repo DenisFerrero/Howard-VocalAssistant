116b71f4a6b9bf687b106fc43d3770a7f133b95e
Denis Ferrero <denis.ferrero@tecnologic-to.it>
2021-09-10 15:35:15 +0200

    Updated dependencies

--- Assistant/src/main.ts
+++ Assistant/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SocketIoAdapter } from './Socket/socket.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Appling adapter to used until Nest use SocketIO v.3
-  app.useWebSocketAdapter(new SocketIoAdapter(app, true));
   await app.listen(4000);
 }
 bootstrap();
